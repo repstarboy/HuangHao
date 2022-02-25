bd8610fdb7486fabeb8da261c1b80e960e11198a
RepStarBoy <2747179089@qq.com>
2022-02-25 18:43:43 +0800

    添加新项目

---
+++ src/main/java/com/example/BingDuDuApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingDuDuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingDuDuApplication.class, args);
+    }
+
+}
